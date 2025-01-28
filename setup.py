5efb4f08cb471ad99b0f45380418d9b87648e1d3
jwtsao <jwtsao1999@gmail.com>
2025-01-28 15:32:38 +0800

    WIP: Package dependency building

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="open-cv",  # 你的套件名稱
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        # 列出你的相依套件
+    ],
+)
---
+++ src/__init__.py
@@ -0,0 +1,6 @@
+from . import oil_painting
+from . import advanced
+from . import cv
+from . import pencil_sketch
+from . import pixel_art
+from . import remove_background
